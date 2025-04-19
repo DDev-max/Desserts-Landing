c1784ff11eb0fe79f42303e85d0d6393d84190f4
Claus <tab88fn@gmail.com>
2025-04-18 19:35:43 -0600

    fix: font family error

--- src/data/fonts.ts
+++ src/data/fonts.ts
@@ -1,3 +1,3 @@
 import { Sour_Gummy } from 'next/font/google'
 
-export const fontFamilyTxt = Sour_Gummy({ weight: ['300'], subsets: ['latin'], display: 'swap' })
+export const fontFamilyTxt = Sour_Gummy({ weight: ['300'], subsets: ['latin'], display: 'swap', adjustFontFallback: false })
